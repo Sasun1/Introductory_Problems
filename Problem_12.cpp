7440dbbf7765b702bc5c8d80ab65291437b88f12
Sasun1 <kostanyan.sasun@gmail.com>
2021-05-02 20:21:10 +0400

    Problem-12

---
+++ Problem_12.cpp
@@ -0,0 +1,16 @@
+// Problem_12.cpp :
+
+#include <iostream>
+
+int main()
+{
+   int a;
+   int b;
+   int c;
+   std::cout << "Dear user please enter any digit:" << std::endl;
+   std::cin >> a >> b >> c;
+   std::cout << a + b + c;
+   return 0;
+}
+
+
