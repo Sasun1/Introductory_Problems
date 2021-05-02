72b638878db410eb97191b5aa131c799c223c4c7
Sasun1 <kostanyan.sasun@gmail.com>
2021-05-02 20:19:34 +0400

    Problem-11

---
+++ Problem_11.cpp
@@ -0,0 +1,18 @@
+// Problem_11.cpp : 
+
+#include <iostream>
+
+int main()
+{
+	short a = 0;
+	int b = 0;
+	double c = 0;
+	std::cout << "Please Enter number until 32767:\t ";
+	std::cin >> a;	
+	std::cout << "Please Enter number until 2147483647:\t ";
+	std::cin >> b;	
+	std::cout << "Please Enter number until 9223372036854775807:\t ";
+	std::cin >> c;
+	return 0;	
+}
+
