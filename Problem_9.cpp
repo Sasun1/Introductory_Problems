dfb41a683d44263c77f34d992b8a87a7b3ce3863
Sasun1 <kostanyan.sasun@gmail.com>
2021-05-02 20:15:19 +0400

    Problem-9

---
+++ Problem_9.cpp
@@ -0,0 +1,14 @@
+// Problem_9.cpp : 
+
+#include <iostream>
+ 
+int main()
+{
+	double x = 12;
+	double y = 23;
+	std::cout << "x + y = "<< x + y << std::endl;
+	std::cout << "x - y = " << x - y << std::endl;
+	std::cout << "x * y = " << x * y << std::endl;
+	std::cout << "x / y = " << x / y << std::endl;
+	return 0;
+}
